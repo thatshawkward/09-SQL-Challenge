948200c862f6e52b8db0b315be6899329d1338eb
thatshawkward <56585064+thatshawkward@users.noreply.github.com>
2020-07-18 04:44:39 -0500

    Create EmployeeSQL.sql

---
+++ EmployeeSQL.sql
@@ -0,0 +1,107 @@
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees
+(
+	emp_no INT PRIMARY KEY NOT NULL,
+	birth_date VARCHAR(30) NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	gender VARCHAR(1) NOT NULL,
+	hire_date VARCHAR(30) NOT NULL
+);
+
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments
+(
+	dept_no VARCHAR(4) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(30) NOT NULL
+);
+
+DROP TABLE IF EXISTS dept_emp;
+CREATE TABLE dept_emp
+(
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	dept_no VARCHAR(4) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	from_date VARCHAR(30) NOT NULL,
+	to_date VARCHAR(30) NOT NULL
+);
+
+DROP TABLE IF EXISTS dept_manager;
+CREATE TABLE dept_manager
+(
+	dept_no VARCHAR(4) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	from_date VARCHAR(30) NOT NULL,
+	to_date VARCHAR(30) NOT NULL
+);
+
+DROP TABLE IF EXISTS salaries;
+CREATE TABLE salaries
+(
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	salary INT NOT NULL,
+	from_date VARCHAR(30) NOT NULL,
+	to_date VARCHAR(30) NOT NULL
+);
+
+DROP TABLE IF EXISTS titles;
+CREATE TABLE titles
+(
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	title VARCHAR(30) NOT NULL,
+	from_date VARCHAR(30) NOT NULL,
+	to_date VARCHAR(30) NOT NULL
+);
+
+SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
+FROM salaries AS s
+INNER JOIN employees AS e ON
+e.emp_no = s.emp_no;
+
+SELECT * FROM employees
+WHERE hire_date LIKE '1986%';
+
+SELECT d.dept_no, d.dept_name, m.emp_no, e.last_name, e.first_name, m.from_date, m.to_date
+FROM departments AS d
+INNER JOIN dept_manager AS m ON
+m.dept_no = d.dept_no
+JOIN employees AS e ON
+e.emp_no = m.emp_no;
+
+SELECT e.emp_no, e.last_name, e.first_name, dp.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS d ON
+e.emp_no = d.emp_no
+INNER JOIN departments AS dp ON
+dp.dept_no = d.dept_no;
+
+SELECT * FROM employees
+WHERE first_name LIKE 'Hercules'
+AND last_name LIKE 'B%';
+
+SELECT e.emp_no, e.last_name, e.first_name, dp.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS d ON
+e.emp_no = d.emp_no
+INNER JOIN departments AS dp ON
+dp.dept_no = d.dept_no
+WHERE dp.dept_name LIKE 'Sales';
+
+SELECT e.emp_no, e.last_name, e.first_name, dp.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS d ON
+e.emp_no = d.emp_no
+INNER JOIN departments AS dp ON
+dp.dept_no = d.dept_no
+WHERE dp.dept_name LIKE 'Development'
+OR dp.dept_name LIKE 'Sales';
+
+SELECT last_name, COUNT(*) AS frequency
+FROM employees
+GROUP BY last_name
+ORDER BY frequency DESC;
